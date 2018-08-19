104fc9b4df3df79f3c6cdc3b90cc40ba44c5d0f4
Flight <flight.neg@gmail.com>
2018-08-19 09:41:02 +0300

    * question main timer added

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -4,7 +4,6 @@ import { ScoreboardService } from '../scoreboard.service';
 import { UserDataService } from '../user-data.service';
 import { HostListener } from '@angular/core';
 import { Router } from '@angular/router';
-import { Timeouts } from 'selenium-webdriver';
 
 @Component({
     selector: 'app-test',
@@ -14,13 +13,20 @@ import { Timeouts } from 'selenium-webdriver';
 export class TestComponent implements OnInit {
     @Input() hasUser: boolean;
 
+    private questionTimeout;
+    private questionCounter = 0;
+
     public question: IQuestion;
-    public startTest = false;
+    public testStarted = false;
     public mixedAnswers = [];
     public testFinished = false;
     public score = 0;
-    private questionCounter = 0;
     public finalSpinnerProgress = 0;
+    public showTimeEndMessage = false;
+    public showQuestionTimer = false;
+
+    public showMainSpinner = false;
+    public mainSpinnerProgress = 0;
 
     constructor(
         private questionService: QuestionService,
@@ -31,7 +37,7 @@ export class TestComponent implements OnInit {
 
     @HostListener('window:keydown', ['$event'])
     onKeyUp(event: KeyboardEvent) {
-        if (this.startTest === true && this.testFinished !== true) {
+        if (this.testStarted === true && this.testFinished !== true) {
             const keyNumber = parseInt(event.key, 10);
             const number = isNaN(keyNumber) ? undefined : keyNumber;
 
@@ -42,7 +48,8 @@ export class TestComponent implements OnInit {
     }
 
     onUserSubmit() {
-        this.startTest = true;
+        this.testStarted = true;
+        this.refreshTimer();
     }
 
     finishTest() {
@@ -50,7 +57,6 @@ export class TestComponent implements OnInit {
         const userData = this.userDataService.get();
         let showScoreTimeout;
         const showScoreTimer = () => {
-            console.log(this.finalSpinnerProgress);
             if (this.finalSpinnerProgress === 110) {
                 clearTimeout(showScoreTimeout);
                 this.router.navigate(['scoreboard', userData.id]);
@@ -61,7 +67,10 @@ export class TestComponent implements OnInit {
             showScoreTimeout = setTimeout(showScoreTimer, finalTimeout / 100);
         };
 
+        clearTimeout(showScoreTimeout);
+
         this.testFinished = true;
+        this.showQuestionTimer = false;
 
         this.userDataService.setScore(this.score);
         this.scoreboardService.addScore(userData);
@@ -69,10 +78,42 @@ export class TestComponent implements OnInit {
         showScoreTimer();
     }
 
-    makeRandomArray(array: Array<any>): Array<any> {
+    private makeRandomArray(array: Array<any>): Array<any> {
         return array.sort(function() {
             return 0.5 - Math.random();
         });
+    }
+
+    private onTimeEnd(): void {
+        this.showQuestionTimer = false;
+        this.showTimeEndMessage = true;
+
+        setTimeout((): void => {
+            this.showTimeEndMessage = false;
+            this.getNextQuestion();
+        }, 5000);
+    }
+
+    private refreshTimer(): void {
+        const questionTimeoutTime = 30000;
+        const mainTimer = () => {
+            if (this.mainSpinnerProgress === 101) {
+                this.onTimeEnd();
+                clearTimeout(this.questionTimeout);
+                return;
+            }
+            this.mainSpinnerProgress++;
+
+            this.questionTimeout = setTimeout(mainTimer, questionTimeoutTime / 100);
+        };
+
+        this.mainSpinnerProgress = 0;
+
+        clearTimeout(this.questionTimeout);
+
+        this.showQuestionTimer = true;
+
+        mainTimer();
     }
 
     getNextQuestion(): void {
@@ -92,6 +133,10 @@ export class TestComponent implements OnInit {
                 this.mixedAnswers = ['True', 'False'];
             } else {
                 this.mixedAnswers = this.makeRandomArray([this.question.correct_answer, ...this.question.incorrect_answers]);
+            }
+
+            if (this.testStarted) {
+                this.refreshTimer();
             }
         });
     }
